757f72ef51a4c5eaac0fd999f5215b6ef4726e7e
JukemJ <chris@dixn.dev>
2022-08-31 20:57:03 -0400

    basic functionality working

--- public/js/main.js
+++ public/js/main.js
@@ -19,6 +19,7 @@ Array.from(todoComplete).forEach((el)=>{
 async function deleteTodo(){
     const todoId = this.parentNode.dataset.id
     try{
+        console.log('this:', todoId)
         const response = await fetch('todos/deleteTodo', {
             method: 'delete',
             headers: {'Content-type': 'application/json'},
@@ -70,3 +71,53 @@ async function markIncomplete(){
         console.log(err)
     }
 }
+
+// Select all slides
+const slides = document.querySelectorAll(".slide");
+
+// Loop through slides and set each slide's translateX property to index * 100%
+slides.forEach()((slide, indx) => {
+    slide.style.transform = `translateX(${indx * 100}%)`;
+});
+
+// Select next slide button
+const nextSlide = document.querySelector(".btn-next");
+
+// Current slide counter
+let curSlide = 0;
+
+//Maximum number of slides
+let maxSlide = slides.length -1;
+
+// Add event listener and navigation functionality
+nextSlide.addEventListener("click", function () {
+    // Check if current slide is the last and reset current slide
+    if (curSlide === maxSlide) {
+        curSlide = 0;
+    } else {
+        curSlide++;
+    }
+
+    // Move slide by -100%
+    slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
+});
+
+// Select prev slide button
+const prevSlide = document.querySelector(".btn-prev");
+
+// Add event listener and navigation functionality
+prevSlide.addEventListener("click", function () {
+    // Check if current slide is the first and reset current slide to last
+    if (curSlide === 0) {
+        curSlide = maxSlide;
+    } else {
+        curSlide--;
+    }
+
+    // Move slide by 100%
+    slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
+});
